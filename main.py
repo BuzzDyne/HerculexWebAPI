14f656b9e0f251cee9be1ab3621c415d78b06837
BuzzDyne <buzzdyne@gmail.com>
2023-06-04 19:27:02 +0700

    Added Active Order Endpoint

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi_jwt_auth import AuthJWT
 from fastapi_jwt_auth.exceptions import AuthJWTException
 from sqlalchemy.orm import Session
 from datetime import timedelta
-from routers import user, order, auth, api_user
+from routers import user, order, auth, api_user, api_order
 from database import get_db
 from database import Order_TM, HCXProcessSyncStatus_TM, User_TM
 from pydantic import BaseModel
@@ -35,6 +35,7 @@ app.include_router(user.router)
 app.include_router(order.router)
 app.include_router(auth.router)
 app.include_router(api_user.router)
+app.include_router(api_order.router)
 
 #region AuthJWT
 class Settings(BaseModel):
---
+++ routers/api_order.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi_jwt_auth import AuthJWT
+from sqlalchemy.orm import Session
+import bcrypt
+
+from database import get_db, Order_TM
+
+
+router = APIRouter(
+    tags=['API Order'],
+    prefix="/api_order"
+)
+
+
+@router.get('/get_active_orders')
+def get_active_orders(db: Session = Depends(get_db)):
+    ecom_status_order_values = [100, 103, 220, 221, 400, 450, 500, 501, 530, 540, 600, 601, 690]
+    res = db.query(Order_TM).filter(
+        Order_TM.ecom_order_status.in_(ecom_status_order_values)).order_by(Order_TM.pltf_deadline_dt.asc()).all()
+    return res
